712e45972d946851fb95904e735fadaa1b434cbe
LuaN1409 <141962309+LuaN1409@users.noreply.github.com>
2023-10-03 05:02:55 -0500

    Vr1

---
+++ src/main/java/vehiculos/Automovil.java
@@ -0,0 +1,24 @@
+package vehiculos;
+
+public class Automovil extends Vehiculo{
+	protected int puestos;
+	protected static int cantidadAutomobiles;
+
+
+	
+	public Automovil(String placa , String nombre, int precio, int peso, Fabricante fabricante, int puestos) {
+		super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
+		this.puestos = puestos;
+		cantidadAutomobiles++;
+	} 
+
+	public static int getCantidadAutomoviles() {
+		  return cantidadAutomobiles;
+	}	
+	public int getPuestos() {
+			return puestos;
+	}
+	public void setPuestos(int puestos) {
+			this.puestos = puestos;
+	}
+}
